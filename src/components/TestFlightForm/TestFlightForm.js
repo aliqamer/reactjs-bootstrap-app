f6bb074e704c50c75f76e72d470a3392b2758358
aliqamer <qameralishaikh@gmail.com>
2020-02-29 08:27:37 +1100

    Added submit function

--- src/components/TestFlightForm/TestFlightForm.js
+++ src/components/TestFlightForm/TestFlightForm.js
@@ -16,11 +16,37 @@ import {
   Input,
   FormText
 } from "reactstrap";
+import Axios from "axios";
 
 class TestFlightForm extends React.Component {
   constructor(props) {
     super(props);
     this.state = { showSuccess: false, showDanger: false };
+    this.handleInputChange = this.handleInputChange.bind(this);
+    this.onSubmit = this.onSubmit.bind(this);
+  }
+
+  handleInputChange(eventData) {
+    const target = eventData.target;
+    const value = target.type === "checkbox" ? target.checked : target.value;
+    const name = target.name;
+    this.setState({ [name]: value });
+  }
+
+  onSubmit(eventData) {
+    eventData.preventDefault();
+    Axios.post("http://localhost:3001/mailingList", {
+      customerName: this.state.customerName,
+      email: this.state.email,
+      phone: this.state.phone,
+      budget: this.state.budget
+    })
+      .then(res => {
+        this.setState({ showSuccess: true, showDanger: false });
+      })
+      .catch(err => {
+        this.setState({ showSuccess: false, showDanger: true });
+      });
   }
 
   render() {
@@ -37,6 +63,7 @@ class TestFlightForm extends React.Component {
               <FormGroup>
                 <Input
                   type="text"
+                  onChange={this.handleInputChange}
                   name="customerName"
                   id="customerName"
                   placeholder="what is your name?"
@@ -46,6 +73,7 @@ class TestFlightForm extends React.Component {
               <FormGroup>
                 <Input
                   type="text"
+                  onChange={this.handleInputChange}
                   name="phone"
                   id="phone"
                   placeholder="what is your phone num?"
@@ -56,6 +84,7 @@ class TestFlightForm extends React.Component {
                 <InputGroupAddon addonType="prepend">@</InputGroupAddon>
                 <Input
                   type="text"
+                  onChange={this.handleInputChange}
                   name="email"
                   id="email"
                   placeholder="what is your email address?"
@@ -66,6 +95,7 @@ class TestFlightForm extends React.Component {
                 <InputGroupAddon addonType="prepend">$</InputGroupAddon>
                 <Input
                   type="text"
+                  onChange={this.handleInputChange}
                   name="budget"
                   id="budget"
                   placeholder="what is your budget?"
@@ -73,11 +103,13 @@ class TestFlightForm extends React.Component {
               </InputGroup>
             </Form>
             <br />
-            <Button>Submit</Button>
-            <Alert color="success">
-              Your data were submitted successfully. Your test flifght awaits{" "}
+            <Button onClick={this.onSubmit}>Submit</Button>
+            <Alert isOpen={this.state.showSuccess} color="success">
+              Your data is submitted successfully. Your test flight awaits{" "}
             </Alert>
-            <Alert color="danger">Something went wrong!</Alert>
+            <Alert isOpen={this.state.showDanger} color="danger">
+              Something went wrong!
+            </Alert>
           </CardBody>
         </Card>
       </div>
